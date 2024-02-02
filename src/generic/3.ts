a54308367ee672b87e575954baf936896d825468
zorianach <zoryanamirchuk@gmail.com>
2024-02-02 23:42:47 +0200

    all tasks

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,7 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge (objA: object, objB: object) {
   return Object.assign(objA, objB);
 }
 
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,16 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface Props {
+title: string;
+}
 
-class Component {
+class Component<T extends Props> {
   constructor (public props:T) {
-
   }
 }
 
-class Page extends Component {
+class Page extends Component<Props> {
   pageInfo () {
     console.log(this.props.title);
   }
